e624713b2b092633f793961e54d95e806e40638d
Elias Freider <elias.freider@kry.se>
2019-12-02 08:16:42 +0100

    Swap part1 and 2. Lint code

--- 02/part1.py
+++ 02/part1.py
@@ -1,26 +1,19 @@
-clean_prg = [int(n) for n in open("input.txt").read().split(",")]
+prg = [int(n) for n in open("input.txt").read().split(",")]
+
+prg[1] = 12
+prg[2] = 2
 
 
-def run(prg):
-    for i in range(len(prg)/4):
-        instr = prg[i*4:(i+1)*4]
-        if instr[0] == 1:
-            prg[instr[3]] = prg[instr[1]] + prg[instr[2]]
-        elif instr[0] == 2:
-            prg[instr[3]] = prg[instr[1]] * prg[instr[2]]
-        elif instr[0] == 99:
-            break
-        else:
-            print("bad instr", instr[0])
-            break
+for i in range(len(prg) // 4):
+    instr = prg[i * 4:(i + 1) * 4]
+    if instr[0] == 1:
+        prg[instr[3]] = prg[instr[1]] + prg[instr[2]]
+    elif instr[0] == 2:
+        prg[instr[3]] = prg[instr[1]] * prg[instr[2]]
+    elif instr[0] == 99:
+        break
+    else:
+        print("bad instr", instr[0])
+        break
 
-
-for a in xrange(100):
-    for b in xrange(100):
-        prg = [x for x in clean_prg]
-        prg[1] = a
-        prg[2] = b
-        run(prg)
-        if prg[0] == 19690720:
-            print(a * 100 + b)
-            exit()
+print(prg[0])
--- 02/part2.py
+++ 02/part2.py
@@ -1,19 +1,26 @@
-prg = [int(n) for n in open("input.txt").read().split(",")]
-
-prg[1] = 12
-prg[2] = 2
+clean_prg = [int(n) for n in open("input.txt").read().split(",")]
 
 
-for i in range(len(prg)/4):
-    instr = prg[i*4:(i+1)*4]
-    if instr[0] == 1:
-        prg[instr[3]] = prg[instr[1]] + prg[instr[2]]
-    elif instr[0] == 2:
-        prg[instr[3]] = prg[instr[1]] * prg[instr[2]]
-    elif instr[0] == 99:
-        break
-    else:
-        print ("bad instr", instr[0])
-        break
+def run(prg):
+    for i in range(len(prg) // 4):
+        instr = prg[i * 4:(i + 1) * 4]
+        if instr[0] == 1:
+            prg[instr[3]] = prg[instr[1]] + prg[instr[2]]
+        elif instr[0] == 2:
+            prg[instr[3]] = prg[instr[1]] * prg[instr[2]]
+        elif instr[0] == 99:
+            break
+        else:
+            print("bad instr", instr[0])
+            break
 
-print prg[0]
+
+for a in range(100):
+    for b in range(100):
+        prg = [x for x in clean_prg]
+        prg[1] = a
+        prg[2] = b
+        run(prg)
+        if prg[0] == 19690720:
+            print(a * 100 + b)
+            exit()
