3c395f26c46b2140f302774499b87a5a10bfda4c
Elias Freider <elias.freider@kry.se>
2019-12-09 09:25:36 +0100

    Clean up

--- 09/part1.py
+++ 09/part1.py
@@ -4,9 +4,6 @@ import itertools
 class ExecutionEnded(Exception):
     pass
 
-
-
-
 class WaitingForInput(Exception):
     pass
 
@@ -18,99 +15,85 @@ class MachineState:
         self.i = 0
         self.rel = 0
         self.INSTR = {
-            1: self.sum,
-            2: self.mult,
-            3: self.inp,
-            4: self.outp,
-            5: self.jumpiftrue,
-            6: self.jumpiffalse,
-            7: self.lessthan,
-            8: self.equals,
-            9: self.adjustrel,
-            99: self.abrt
+            1: (self.sum, 3),
+            2: (self.mult, 3),
+            3: (self.inp, 1),
+            4: (self.outp, 1),
+            5: (self.jumpiftrue, 2),
+            6: (self.jumpiffalse, 2),
+            7: (self.lessthan, 3),
+            8: (self.equals, 3),
+            9: (self.adjustrel, 1),
+            99: (self.abrt, 0)
         }
 
-    def read(self, arg, mode):
+    def read(self, j):
+        return self.prg[j]
+
+    def write(self, j, v):
+        self.prg[j] = v
+
+    def adjustrel(self, j):
+        self.rel += self.read(j)
+
+    def sum(self, j, k, l):
+        self.write(l, self.read(j) + self.read(k))
+
+    def mult(self, j, k, l):
+        self.write(l, self.read(j) * self.read(k))
+
+    def abrt(self):
+        raise ExecutionEnded()
+
+    def inp(self, j):
+        if not self.inv:
+            raise WaitingForInput()
+
+        self.write(j, self.inv.pop(0))
+
+    def outp(self, j):
+        o = self.read(j)
+        self.outv.append(o)
+
+    def jumpiftrue(self, j, k):
+        if self.read(j):
+            self.i = self.read(k)
+
+    def jumpiffalse(self, j, k):
+        if not self.read(j):
+            self.i = self.read(k)
+
+    def lessthan(self, j, k, l):
+        self.write(l, int(self.read(j) < self.read(k)))
+
+    def equals(self, j, k, l):
+        self.write(l, int(self.read(j) == self.read(k)))
+
+    def get_cursor_pos(self, rawpos, mode):
         if mode == "0":
-            return self.prg[arg]
+            return self.prg[rawpos]
         elif mode == "1":
-            return arg
+            return rawpos
         elif mode == "2":
-            return self.prg[self.rel + arg]
+            return self.prg[rawpos] + self.rel
         else:
             print(f"BAD MODE {mode}")
 
-    def write(self, arg, mode, v):
-        if mode == "0":
-            self.prg[arg] = v
-        elif mode == "1":
-            print(f"Can't write with mode={mode}")
-        elif mode == "2":
-            self.prg[self.rel + arg] = v
-
-    def adjustrel(self, modes):
-        a, = list(zip(self.prg[self.i+1: self.i+2], modes))
-        self.rel += self.read(*a)
-        return self.i+2
-
-    def sum(self, modes):
-        a, b, c = list(zip(self.prg[self.i+1: self.i+4], modes))
-        self.write(c[0], c[1], self.read(*a) + self.read(*b))
-        return self.i+4
-
-    def mult(self, modes):
-        a, b, c = list(zip(self.prg[self.i+1: self.i+4], modes))
-        self.write(c[0], c[1], self.read(*a) * self.read(*b))
-        return self.i+4
-
-    def abrt(self, modes):
-        raise ExecutionEnded()
-
-    def inp(self, modes):
-        if not self.inv:
-            raise WaitingForInput()
-        a, = list(zip(self.prg[self.i+1:self.i+2], modes))
-
-        self.write(a[0], a[1], self.inv.pop(0))
-        return self.i+2
-
-    def outp(self, modes):
-        a, = list(zip(self.prg[self.i+1:self.i+2], modes))
-        o = self.read(*a)
-        self.outv.append(o)
-        return self.i+2
-
-    def jumpiftrue(self, modes):
-        a, b = list(zip(self.prg[self.i+1: self.i+3], modes))
-        if self.read(*a):
-            return self.read(*b)
-        return self.i + 3
-
-    def jumpiffalse(self, modes):
-        a, b = list(zip(self.prg[self.i+1: self.i+3], modes))
-        if not self.read(*a):
-            return self.read(*b)
-        return self.i + 3
-
-    def lessthan(self, modes):
-        a, b, c = list(zip(self.prg[self.i+1: self.i+4], modes))
-        self.write(c[0], c[1], int(self.read(*a) < self.read(*b)))
-        return self.i+4
-
-    def equals(self, modes):
-        a, b, c = list(zip(self.prg[self.i+1: self.i+4], modes))
-        self.write(c[0], c[1], int(self.read(*a) == self.read(*b)))
-        return self.i+4
-
     def exec(self):
         while 1:
-            raw_op = self.prg[self.i]
+            i = self.i
+            raw_op = self.prg[i]
             opcode = raw_op % 100
-            op = self.INSTR[opcode]
+            op, numargs = self.INSTR[opcode]
             raw_mode = str(raw_op)[:-2]
-            modes = ("0" * (3 - len(raw_mode)) + raw_mode)[::-1]
+            modes = ("0" * (numargs - len(raw_mode)) + raw_mode)[::-1]
+            args = range(self.i+1, self.i+1+numargs)
+            cursors = [self.get_cursor_pos(*t) for t in zip(args, modes)]
+
             try:
-                self.i = op(modes)
+                op(*cursors)
+                if i == self.i:
+                    self.i += 1 + numargs
             except ExecutionEnded:
                 break
 
