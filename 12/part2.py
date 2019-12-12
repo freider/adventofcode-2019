8a185c7b9eb294d0168f75d36a46ab5ccf9eca33
Elias Freider <elias.freider@kry.se>
2019-12-12 08:20:12 +0100

    Clean up d10 part 2 (assume return to origin)

--- 12/part2.py
+++ 12/part2.py
@@ -9,33 +9,24 @@ moons = np.array([
 ])
 
 def calc(orig):
-    mem = set()
     pos = orig.copy()
     velos = np.zeros(pos.shape, dtype=int)
+    origstate = (tuple(pos), tuple(velos))
     i = 0
-    a = None
-    b = None
-    origall = (tuple(pos), tuple(velos))
-    mem = {origall}
     while 1:
         for m in range(4):
             diff = pos - pos[m]
             vchange = diff//np.abs(diff)
             velos[m] += vchange.sum(axis=0)
         pos += velos
-
         i += 1
-        tup = (tuple(pos), tuple(velos))
-        if tup in mem and a is None:
-            a = i
-        if tup == origall:
-            b = i
-            return a, b
-        mem.add(tup)
+        state = (tuple(pos), tuple(velos))
+        if state == origstate:
+            return i
 
-a, b = calc(moons[:, 0])
-c, d = calc(moons[:, 1])
-e, f = calc(moons[:, 2])
+a = calc(moons[:, 0])
+c = calc(moons[:, 1])
+e = calc(moons[:, 2])
 
 # a, c, e = [161428, 231614, 108344]  # from above calc, took a while to run
 
