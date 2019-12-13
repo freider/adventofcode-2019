0ee684d1354d299819538c32b825cec8dd5524ba
Elias Freider <elias.freider@kry.se>
2019-12-13 21:04:55 +0100

    Updated template and tests

--- lib/parse__test.py
+++ lib/parse__test.py
@@ -9,3 +9,8 @@ def test_float_dict():
     rp = ReParse(r"<x={x:float}, y={y:int}, z=-{z:str}>")
     assert {"x": -9.0, "y": 10, "z": "1"} == rp.match("<x=-9, y=10, z=-1>")
 
+def test_suffix_match():
+    assert (15,) == ReParse(r"{int}").match("15trololol")
+
+def test_parse_multi():
+    assert (1,2,3) == ReParse(r"({int},?)*").match("1,2,3")
--- template/part1.py
+++ template/part1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import array as V
-from lib.npdraw import *
+from lib.npdraw import draw, sparse_to_array
 from lib.machine import Machine, IterMachine
 from lib.parse import ReParse
 
