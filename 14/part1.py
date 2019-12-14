614e5cd7dd67b89a4bec4c25ea5db4350c7630ec
Elias Freider <elias.freider@kry.se>
2019-12-14 10:30:37 +0100

    Working part1

--- 14/part1.py
+++ 14/part1.py
@@ -9,7 +9,7 @@ def tod(inglist):
     return {v: int(k) for k, v in inglist}
 
 def read():
-    for line in open("14/input-test.txt"):
+    for line in open("14/input.txt"):
         from_in, to_in = [p.strip() for p in line.split("=>")]
         sep = [s.strip() for s in from_in.split(", ")]
         ing_list = [s.split() for s in sep]
@@ -18,30 +18,37 @@ def read():
 
 recipes = list(read())
 
-def build(ing, n):
+def build(ing, n, inventory):
     if ing == "ORE":
-        return n, {}
+        return n, inventory
+    
+    new_inventory = inventory.copy()
+
+    if ing in inventory:
+        used_inv = min(new_inventory[ing], n)
+        n -= used_inv
+        new_inventory[ing] -= used_inv
 
     for requires, produces in recipes:
         if ing in produces:
             numproduced = produces[ing]
             required_processes = (n + numproduced -1) // numproduced
+
             extras_produced = required_processes * numproduced - n
             print(f"{n} {ing} requires {required_processes} * {requires} (extras:{extras_produced})")
 
             s = 0
-            all_fluff = defaultdict(int)
-            
             for k, v in requires.items():
-                cost, fluff = build(k, v * required_processes)
-                for a, b in fluff.items():
-                    all_fluff[a] += b
-
+                cost, new_inventory = build(k, v * required_processes, new_inventory)
                 s += cost
 
-            if extras_produced:
-                all_fluff[ing] = extras_produced
+            new_inventory[ing] += extras_produced
 
-            return s, all_fluff
+            return s, new_inventory
 
-print(build("FUEL", 1))
+print(build("FUEL", 1, defaultdict(int)))
+#print()
+#start = defaultdict(int)
+# start["A"] = 1
+# start["B"] = 1
+#print(build("AB", 1, start))
